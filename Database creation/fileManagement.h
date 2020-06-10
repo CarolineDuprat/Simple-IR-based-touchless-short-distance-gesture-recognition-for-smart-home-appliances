aa96412d99c6455f5c3256eab687b23277f0ac1d
Caroline <dupratcaroline78@gmail.com>
2020-06-10 14:13:36 +0200

    Revert "Merge branch 'develop'"
    This reverts commit 2b8d6b5f32e0850fbcaf255c328858c3722b4376.

---
+++ Database creation/fileManagement.h
@@ -0,0 +1,12 @@
+#ifndef FILEMANAGEMENT_H
+#define FILEMANAGEMENT_H
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <math.h>
+#include <vector>
+
+bool writeFile (std::string nameFile,double data [][3],int nbrID);
+
+#endif // FILEMANAGEMENT_H
