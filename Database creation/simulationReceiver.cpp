491c32f48820bfdc929271f164038a124da07f89
Caroline <dupratcaroline78@gmail.com>
2020-06-17 17:17:37 +0200

    Add comments

--- Database creation/simulationReceiver.cpp
+++ Database creation/simulationReceiver.cpp
@@ -9,13 +9,19 @@
 using namespace std;
 /**
  * @brief simulationTime Calcul the total time of the simulation [ms]
+ * The object must travel from (x,y) to (-x,-y). So the total distance is the initial distance * 2
+ * We know also the speed of the object.
+ * So we can know the total time of the simulation : t = (total distance ) / vitesse
  * @param distance distance from object center to device center [cm]
  * @param speed speed of the object [m/s]
  * @return the total time of the simulation [ms]
  */
 int simulationTime (double const distance,double const speed){
-    // speed[m/s] = speed[cm/ms]*1/10
+    // Conversion of m/s to cm/ms : speed[m/s] = speed[cm/ms]*1/10
+    // t [ms] = (total distance [cm] ) / vitesse [cm/ms] = ( 2 * distance [cm] ) / ( vitesse [m/s] * 1/10 )
+    //                                                   = 10 * ( 2 * distance [cm] ) / ( vitesse [m/s] )
     int time=10*(distance*2)/speed;
+    // the result of the calcul " 10*(distance*2)/speed " is a double not a int, so when we write " int time " we lose the decimal part
     //example: if reel time is 5.6 => time=5; but we want to travel the whole distance => time = time+1
     if (time < 10*(distance*2)/speed){
         time += 1;
@@ -24,6 +30,9 @@ int simulationTime (double const distance,double const speed){
 }
 /**
  * @brief HorizontalAndVerticalSpeed Calcul the horizontal and vertical speed and the initial position of the object
+ * Initial position : We know the distance of the object and the angular position of the object, so we can project this point to know its position in the orthormed frame
+ * Horizontal and vertical speed : The calculation of these speeds makes it easier to know the position of the object at all times
+ *                                 We know the total distance and the total time => vitesse = distance / time
  * @param distance distance from object center to device center [cm]
  * @param speed speed of the object [m/s]
  * @param angle Angular position of the obstacle [°]
@@ -35,25 +44,35 @@ returnSpeedPos HorizontalAndVerticalSpeed (double const distance,double const sp
     int totalTime;
     returnSpeedPos retour;
 
-    //Convert degree to radian
+    // Convert degree to radian
     angleRad = angle * PI / 180;
 
-    //Initial position
+    // Initial position
+    // cos = adjacent / hypotenuse = xInit / distance
     double xInit = distance*cos(angleRad);
     retour.pos.x=xInit;
+    // sin = opposite / hypotenuse = yInit / distance
     double yInit = distance*sin(angleRad);
     retour.pos.y=yInit;
 
-    //Total speed [ms]
+    // Total speed [ms]
     totalTime=simulationTime (distance,speed);
 
-    //HorizontalSpeed [cm/ms]
+    // HorizontalSpeed [cm/ms]
+    // vitesse = distance / time
+    // the total time of the simulation is calcul in the function simulationTime, the result is totalTime
+    // the total distance is |xInit| * 2
+    // Vitesse horizontal =  |xInit| * 2  / totalTime
+    // The sens of the vitesse allows to know is the object moves to increasing or decreasing x
+    // If (-xInit)>xInit , the object moves to increasing x
     if ((-xInit)>xInit){
         retour.speed.hor=2*fabs(xInit)/totalTime;
-    }else{
+    }// Else, the object moves to decreasing x
+    else{
         retour.speed.hor=-(2*fabs(xInit)/totalTime);
     }
-    //VerticalSpeed [cm/ms]
+    // VerticalSpeed [cm/ms]
+    // Vitesse horizontal =  |yInit| * 2  / totalTime
     if ((-yInit)>yInit){
         retour.speed.vert=2*fabs(yInit)/totalTime;
     }else{
@@ -64,9 +83,11 @@ returnSpeedPos HorizontalAndVerticalSpeed (double const distance,double const sp
 }
 /**
  * @brief positionMoveObject Calcul the position of the object
+ * In the function HorizontalAndVerticalSpeed, we calculated the initial position of the object, as well as the horizontal and vertical speeds
+ * So we can calculate the position traveled by the object at all times, and add this distance to the initial position to know the position of the object
  * @param information return of the function HorizontalAndVerticalSpeed :
  *         information.pos position of the object [cm]
- *         information.speed : horizontal and vertical speed [cm/ms]
+ *         information.speed : horizontal and vertical speeds [cm/ms]
  * @param time time of the simulation [ms]
  * @return positionObject [cm]
  */
@@ -74,14 +95,17 @@ position_t positionMoveObject (returnSpeedPos information ,unsigned int time){
     position_t positionObject;
 
     // x Object = x Intial Object + HorizontalSpeed * time
-    positionObject.x=information.pos.x+information.speed.hor*time;
+    positionObject.x = information.pos.x + information.speed.hor*time;
     // y Object = y Intial Object + VerticalSpeed * time
-    positionObject.y=information.pos.y+information.speed.vert*time;
+    positionObject.y = information.pos.y + information.speed.vert*time;
 
     return positionObject;
 }
 /**
  * @brief rotationCorner Center of the object (0,0), Obtained position of corners after rotation
+ * To know the position of the corners of the rectangle, we will first assume that the rectangle
+ * is aligned with the x and y axes, so it will be easy to calculate the position of the corners
+ * with the dimension of the object. Then we will apply a rotation to these points (matrix rotation)
  * @param dimensionObject dimension of the object [cm]
  * @param angle Angular position of the obstacle [°]
  * @return position of corners
@@ -90,6 +114,7 @@ std::vector<position_t> rotationCorner (dimension dimensionObject,int angle){
     std::vector<position_t> positionCorner(4),positionCornerRotation(4);
     double rotRad;
 
+    //Assume that the rectangle is aligned with the x and y axes
     //Position c1 : (l/2,w/2);
     positionCorner[0].x=dimensionObject.length/2;
     positionCorner[0].y=dimensionObject.width/2;
@@ -108,6 +133,8 @@ std::vector<position_t> rotationCorner (dimension dimensionObject,int angle){
 
     // Rotation matrix : ( xProj )   =   (  cos (angle)     - sin (angle)   ) ( dx )
     //                   ( yProj )       (  sin (angle)       cos (angle)   ) ( dy )
+    // xProj = cos (angle)*dx + (- sin (angle))*dy
+    // yProj = sin (angle)*dx + cos (angle)*dy
 
     //Position Rotation :
     for (int i(0);i<4;i++){
@@ -161,6 +188,10 @@ bool ReceiverCovered (position_t positionReceiver,std::vector<position_t> positi
 }
 /**
  * @brief ReelPositionCorner translation of the origin
+ * In the function rotationCorner, we calculated the position of the corners assuming that the center
+ * of the rectangle is (0,0), except that the position of the object is not that, it changes with time
+ * we calculate the position of the object in the function positionMoveObject
+ * so the actual position of the corners is positionCorner + positionObject
  * @param positionObject positionObject [cm]
  * @param positionCornerRotation position corner after the rotation, the origin is the center of the object [cm]
  * @return  reel position of corners [cm]
